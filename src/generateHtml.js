c41846e6133c1d5b2044a56bf35c7bc0f31f382d
sonjailic <sonjailic94@hotmail.com>
2022-08-10 18:11:54 -0400

    add intern to display on page

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -7,6 +7,21 @@ module.exports = generatePage = (employee) => {
         if (employee[i].getRole() === "Manager") {
             team.push(` 
             <!DOCTYPE html>
+            <html lang="en">
+            <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+                rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+                crossorigin="anonymous">
+            <link rel="stylesheet" href="./assets/style.css">
+            <!--This is the banner-->
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1 class="display-4">My Team</h1>
+                </div>
+            </div>
             <div class="card" style="width: 10rem;">
                 <div class="card-body">         
                     <h5 class="card-title">${employee[i].getName()}</h5>
@@ -30,34 +45,34 @@ module.exports = generatePage = (employee) => {
                     <ul>
                     <li class="employee-id">ID: ${employee[i].getId()} </li>
                     <li class="employee-email">Email: <a href="mailto:${employee[i].getEmail()}">${employee[i].getEmail()}</a></li>
-                    <li class="office"> GitHub: ${employee[i].getGithub()} </li>
+                    <li class="git"> GitHub: ${employee[i].getGithub()} </li>
                     </p>
                 </div>
         </div>
         </HTML>
         `)
-        };
+        } else if (employee[i].getRole() === "Intern") {
+            team.push(` 
+            <div class="card" style="width: 10rem;">
+                <div class="card-body">         
+                    <h5 class="card-title">${employee[i].getName()}</h5>
+                    <p class="card-text">
+                    <ul>
+                    <li class="employee-id">ID: ${employee[i].getId()} </li>
+                    <li class="employee-email">Email: <a href="mailto:${employee[i].getEmail()}">${employee[i].getEmail()}</a></li>
+                    <li class="school"> School: ${employee[i].getSchool()} </li>
+                    </p>
+                </div>
+        </div>
+        </HTML>
+        `)
+        }
     }
     return team.join("");
 
 
     //     return `
-    //     <!DOCTYPE html>
-    //     <html lang="en">
-    //     <head>
-    //     <meta charset="UTF-8">
-    //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    //     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    //     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
-    //         rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
-    //         crossorigin="anonymous">
-    //     <link rel="stylesheet" href="./assets/style.css">
-    //     <!--This is the banner-->
-    //     <div class="jumbotron jumbotron-fluid">
-    //         <div class="container">
-    //             <h1 class="display-4">My Team</h1>
-    //         </div>
-    //     </div>
+
     //     </head>
 
     //     <body>
